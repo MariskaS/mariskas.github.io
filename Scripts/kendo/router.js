4076cd4e4e2da10b991c6e070ddb4e0a7b9fa12c
MariskaS <marina1988sherbakova@gmail.com>
2015-08-03 01:35:28 +0300

    master: косметика

---
+++ Scripts/kendo/router.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+
+	var router = new kendo.Router(); //router - маршрутизатор(управляет маршрутами приложения)
+
+	router.route("/template", function () {
+		// show preloader
+		$.ajax({
+			url: "/template/aboutUs.html",
+			success: function (data) {
+				// hide preloader
+				// render content
+
+				var aboutUs = new kendo.View(
+				data, { model: kendo.observable({ foo: "О нас" }) });
+
+				$("#view").html(aboutUs.render());
+				$("body").removeClass('home');
+				$("body").addClass('no-slider');
+			},
+			error: function () {
+				// hide preloader
+				// show error
+			}
+		});
+	});
+
+	router.start();
+
+	$("#aboutUs").click(function (e) {
+		e.preventDefault();
+		router.navigate("/template");
+	});
+});
---
+++ Scripts/ui.js
@@ -0,0 +1,43 @@
+﻿
+$(function () {
+// 4 Bootstrap
+
+	//if nav-bar fix top
+	$('#mainNav').affix({
+		offset: {
+			top: 100
+		}
+	});
+
+	//tooltip
+	$('[data-toggle="tooltip"]').tooltip();
+
+	//popover
+	$('[data-toggle="popover"]').popover();
+
+// END Bootstrap
+
+//show menu right or left
+	$("#menuLeft").click(function (e) {
+		e.preventDefault();
+		$("body").toggleClass("show-sidebar-left");
+	});
+
+	$("#menuRight").click(function (e) {
+		e.preventDefault();
+		$("body").toggleClass("show-sidebar-right");
+	});
+
+	$(".overlay").click(function () {
+		$("body").removeClass("show-sidebar-right");
+	});
+
+	//carousel
+		$('.carousel').carousel({
+			interval: 6000
+		});
+
+
+
+});
+
