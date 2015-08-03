4fa2aab0d2829de8ba9d30c40e38a540f1db1617
mscherbakova <mscherbakova@esphere.ru>
2015-08-03 12:55:04 +0300

    master:  add new pages

--- Scripts/kendo/router.js
+++ Scripts/kendo/router.js
@@ -2,20 +2,17 @@
 
 	var router = new kendo.Router(); //router - маршрутизатор(управляет маршрутами приложения)
 
-	router.route("/template", function () {
-		// show preloader
+	router.route("/thanks", function () {
 		$.ajax({
-			url: "/template/aboutUs.html",
+			url: "template/thanks/thanks.html",
 			success: function (data) {
 				// hide preloader
 				// render content
 
-				var aboutUs = new kendo.View(
-				data, { model: kendo.observable({ foo: "О нас" }) });
+				var thanks = new kendo.View(
+					data, { model: kendo.observable({ foo: "Благодарности" }) });
 
-				$("#view").html(aboutUs.render());
-				$("body").removeClass('home');
-				$("body").addClass('no-slider');
+				$("#view").html(thanks.render())
 			},
 			error: function () {
 				// hide preloader
@@ -24,10 +21,131 @@
 		});
 	});
 
+	router.route("/news", function () {
+		$.ajax({
+			url: "template/news/news.html",
+			success: function (data) {
+				// hide preloader
+				// render content
+
+				var news = new kendo.View(
+					data, { model: kendo.observable({ foo: "Новости" }) });
+
+				$("#view").html(news.render())
+			},
+			error: function () {
+				// hide preloader
+				// show error
+			}
+		});
+	});
+
+	router.route("/events", function () {
+		$.ajax({
+			url: "template/events/events.html",
+			success: function (data) {
+				// hide preloader
+				// render content
+
+				var events = new kendo.View(
+					data, { model: kendo.observable({ foo: "Мероприятия" }) });
+
+				$("#view").html(events.render())
+			},
+			error: function () {
+				// hide preloader
+				// show error
+			}
+		});
+	});
+
+	router.route("/what_to_do", function () {
+		$.ajax({
+			url: "template/what_to_do/what_to_do.html",
+			success: function (data) {
+				// hide preloader
+				// render content
+
+				var whatToDo = new kendo.View(
+					data, { model: kendo.observable({ foo: "Что сделанно" }) });
+
+				$("#view").html(whatToDo.render())
+			},
+			error: function () {
+				// hide preloader
+				// show error
+			}
+		});
+	});
+
+	router.route("/reports", function () {
+		$.ajax({
+			url: "template/reports/reports.html",
+			success: function (data) {
+				// hide preloader
+				// render content
+
+				var reports = new kendo.View(
+					data, { model: kendo.observable({ foo: "Отчеты" }) });
+
+				$("#view").html(reports.render())
+			},
+			error: function () {
+				// hide preloader
+				// show error
+			}
+		});
+	});
+
+	router.route("/directory", function () {
+		$.ajax({
+			url: "template/directory/directory.html",
+			success: function (data) {
+				// hide preloader
+				// render content
+
+				var directory = new kendo.View(
+					data, { model: kendo.observable({ foo: "Справочник" }) });
+
+				$("#view").html(directory.render())
+			},
+			error: function () {
+				// hide preloader
+				// show error
+			}
+		});
+	});
+
+
 	router.start();
 
-	$("#aboutUs").click(function (e) {
+	$("#thanks").click(function (e) {
 		e.preventDefault();
-		router.navigate("/template");
+		router.navigate("/thanks");
+	});
+
+	$("#news").click(function (e) {
+		e.preventDefault();
+		router.navigate("/news");
+	});
+
+	$("#events").click(function (e) {
+		e.preventDefault();
+		router.navigate("/events");
+	});
+
+	$("#whatToDo").click(function (e) {
+		e.preventDefault();
+		router.navigate("/what_to_do");
+	});
+
+	$("#reports").click(function (e) {
+		e.preventDefault();
+		router.navigate("/reports");
+	});
+
+	$("#directory").click(function (e) {
+		e.preventDefault();
+		router.navigate("/directory");
 	});
 });
---
+++ Scripts/testTemplate.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mscherbakova on 03.08.2015.
+ */
+
+
+
+$(function () {
+
+});
