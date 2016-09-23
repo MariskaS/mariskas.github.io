4fc07fb457baa640fa2ec2644338a35edf1d0b06
Marina Scherbakova <m.scherbakova@constructivesoftware.com.au>
2016-09-24 01:12:58 +0300

    master: My First Angular App

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,6 @@
-import {Component} from 'angular2/core'; // импортим функцию Component из библиотеки core
-//[public || private || static] [property-name] [:property-type] [= value]
-export class Hero {
-    id: number;
-    name: string;
-}
-
-@Component({ // декоратор или правильнее компонент. Внутри методанные
+import { Component } from '@angular/core';
+@Component({
     selector: 'my-app',
-    template:`
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-    </div>`
+    template: '<h1>My First Angular App</h1>'
 })
-
-export class AppComponent {
-    title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-}
-
+export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
+import { AppComponent }   from './app.component';
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
-    declarations: [
-        AppComponent
-    ],
-    bootstrap: [ AppComponent ]
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
