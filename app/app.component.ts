c4d3110673abaa962f3715cedbf0c667eee4b9a3
Marina Scherbakova <m.scherbakova@constructivesoftware.com.au>
2016-09-25 03:44:15 +0300

    master: Multiple Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-export class Hero {
-    id: number;
-    name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
     { id: 11, name: 'Mr. Nice' },
@@ -16,30 +13,22 @@ const HEROES: Hero[] = [
     { id: 19, name: 'Magma' },
     { id: 20, name: 'Tornado' }
 ];
-// ngIf & ngFor - are called “structural directives” because they can change the structure of portions of the DOM
+
 @Component({
     selector: 'my-app',
     template: `
+    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-        <li *ngFor="let hero of heroes"
-            [class.selected]="hero === selectedHero"
-            (click)="onSelect(hero)">
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-
-    `
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `
 })
-
 export class AppComponent {
     title = 'Tour of Heroes';
     heroes = HEROES;
